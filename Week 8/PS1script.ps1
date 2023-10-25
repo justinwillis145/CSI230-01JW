d065167a710324caa65687eff2ae58bce2794678
justinwillis145 <72421206+justinwillis145@users.noreply.github.com>
2023-10-25 17:42:36 -0400

    Update PS1script.ps1

--- Week 8/PS1script.ps1
+++ Week 8/PS1script.ps1
@@ -1,3 +1,10 @@
+#Storyline: This script prompts the user to select an event log to export, retrieves the three most recent entries from the specified event log, 
+# and exports those entries to a CSV file on the desktop.
+
 Get-EventLog -List
+
+#Prompts the user to get retrieve one of the log files
 $expLog = Read-Host -Prompt "Please select a log to export!"
+
+#retrieves the three most recent entries from the event log specified by the user and exports them to a file on the desktop
 Get-EventLog -LogName $expLog -Newest 3 | Export-Csv -NoTypeInformation -Path "C:\Users\champuser\Desktop\rep.csv"
